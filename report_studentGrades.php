d71d7a690fc9f7cd7b983494016e3f6fb6242a38
Sandra Kuipers <Sandra.Kuipers@tis.edu.mo>
2017-04-27 14:14:22 +0800

    Added students grades page to help with Course Approval process

---
+++ report_studentGrades.php
@@ -0,0 +1,116 @@
+<?php
+/*
+Gibbon, Flexible & Open School System
+Copyright (C) 2010, Ross Parker
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+use Gibbon\Modules\CourseSelection\Domain\SelectionsGateway;
+use Gibbon\Modules\CourseSelection\Domain\GradesGateway;
+
+// Autoloader & Module includes
+$loader->addNameSpace('Gibbon\Modules\CourseSelection\\', 'modules/Course Selection/src/');
+include "./modules/" . $_SESSION[$guid]["module"] . "/moduleFunctions.php" ;
+
+if (isActionAccessible($guid, $connection2, '/modules/Course Selection/approval_byOffering.php') == false) {
+    //Acess denied
+    echo "<div class='error'>" ;
+        echo __('You do not have access to this action.');
+    echo "</div>" ;
+} else {
+    echo "<div class='trail'>" ;
+    echo "<div class='trailHead'><a href='" . $_SESSION[$guid]["absoluteURL"] . "'>" . __($guid, "Home") . "</a> > <a href='" . $_SESSION[$guid]["absoluteURL"] . "/index.php?q=/modules/" . getModuleName($_GET["q"]) . "/" . getModuleEntry($_GET["q"], $connection2, $guid) . "'>" . __($guid, getModuleName($_GET["q"])) . "</a> > </div><div class='trailEnd'>" . __($guid, 'Student Grades', 'Course Selection') . "</div>" ;
+    echo "</div>" ;
+    echo "<br/>" ;
+
+    if (isset($_GET['return'])) {
+        returnProcess($guid, $_GET['return'], null, null);
+    }
+
+    $gibbonPersonIDStudent = isset($_REQUEST['gibbonPersonIDStudent'])? $_REQUEST['gibbonPersonIDStudent'] : 0;
+
+    $selectionsGateway = new SelectionsGateway($pdo);
+    $gradesGateway = new GradesGateway($pdo);
+
+    $studentResults = $selectionsGateway->selectStudentDetails($gibbonPersonIDStudent);
+    $studentEnrolmentResults = $gradesGateway->selectStudentEnrolmentByStudent($gibbonPersonIDStudent);
+
+    if ($studentResults->rowCount() == 0 || $studentEnrolmentResults->rowCount() == 0) {
+        echo '<div class="error">';
+        echo __("There are no records to display.") ;
+        echo '</div>';
+    } else {
+        $student = $studentResults->fetch();
+
+        echo '<br/>';
+        echo '<div>';
+        echo __('Viewing report grades for ');
+        echo '<a href="'.$_SESSION[$guid]['absoluteURL'].'/index.php?q=/modules/Students/student_view_details.php&gibbonPersonID='.$gibbonPersonIDStudent.'" target="_blank">';
+        echo '<strong>'.formatName('', $student['preferredName'], $student['surname'], 'Student', false, true).'</strong>';
+        echo '</a>';
+        echo '</div>';
+
+        while ($enrolment = $studentEnrolmentResults->fetch())
+        {
+            $gradesResults = $gradesGateway->selectStudentReportGradesBySchoolYear($enrolment['schoolYearID'], $gibbonPersonIDStudent);
+
+            if (!$gradesResults || $gradesResults->rowCount() == 0) continue;
+
+            echo '<h3>';
+            echo $enrolment['schoolYearName'];
+            echo '</h3>';
+
+            echo '<table class="reportGradesTable colorOddEven" cellspacing="0">';
+            echo '<tr class="break">';
+                echo '<td>'.__('Course').'</td>';
+                echo '<td>'.__('Name').'</td>';
+                echo '<td>'.__('Sem1-Mid').'</td>';
+                echo '<td>'.__('Sem1-End').'</td>';
+                echo '<td>'.__('Sem2-Mid').'</td>';
+                echo '<td>'.__('Sem2-End').'</td>';
+                echo '<td style="border-left: 2px solid #bbb;">'.__('Exam').'</td>';
+                echo '<td>'.__('Final').'</td>';
+            echo '</tr>';
+
+            $schoolYearGrades = $gradesGateway->processStudentGrades($enrolment, $gradesResults->fetchAll());
+
+            foreach ($schoolYearGrades as $courseGrades) {
+
+                $sem1Mid = (!empty($courseGrades['Sem1-Mid']))? $courseGrades['Sem1-Mid'].'%' : '';
+                $sem1End = (!empty($courseGrades['Sem1-End']))? $courseGrades['Sem1-End'].'%' : '';
+                $sem2Mid = (!empty($courseGrades['Sem2-Mid']))? $courseGrades['Sem2-Mid'].'%' : '';
+                $sem2End = (!empty($courseGrades['Sem2-End']))? $courseGrades['Sem2-End'].'%' : '';
+                $credits = (!empty($courseGrades['credits']))? $courseGrades['credits'].'%' : '';
+                $exam = (!empty($courseGrades['Exam']))? $courseGrades['Exam'].'%' : '';
+                $final = (!empty($courseGrades['Final']))? $courseGrades['Final'].'%' : '';
+
+                echo '<tr>';
+                    echo '<td class="gradesRow" style="width:15%;text-align:left;">'.$courseGrades['courseNameShort'].'</td>';
+                    echo '<td class="gradesRow" style="width:37%;text-align:left;">'.$courseGrades['courseName'].'</td>';
+                    echo '<td class="gradesRow" style="width:8%;">'.$sem1Mid.'</td>';
+                    echo '<td class="gradesRow" style="width:8%;">'.$sem1End.'</td>';
+                    echo '<td class="gradesRow" style="width:8%">'.$sem2Mid.'</td>';
+                    echo '<td class="gradesRow" style="width:8%">'.$sem2End.'</td>';
+                    echo '<td class="gradesRow" style="width:8%;border-left: 2px solid #bbb;">'.$exam.'</td>';
+                    echo '<td class="gradesRow" style="width:8%;text-align:left;">'.$final.'</td>';
+                echo '</tr>';
+            }
+            echo '</table>';
+
+
+        }
+    }
+
+}
--- src/Domain/GradesGateway.php
+++ src/Domain/GradesGateway.php
@@ -76,8 +76,123 @@ class GradesGateway
                 AND FIND_IN_SET(gibbonCourse.gibbonDepartmentID, :gibbonDepartmentIDList)
                 AND arrLegacyGrade.reportTerm='Final'
                 ) ORDER BY schoolYearName, courseOrder, courseNameShort";
-        $result = $this->pdo->executeQuery($data, $sql);
 
-        return $result;
+        return $this->pdo->executeQuery($data, $sql);
+    }
+
+    public function selectStudentReportGradesBySchoolYear($gibbonSchoolYearID, $gibbonPersonID) {
+        $data = array(
+            'gibbonPersonID' => $gibbonPersonID,
+            'gibbonSchoolYearID' => $gibbonSchoolYearID
+        );
+
+        $sql = "(SELECT (CASE WHEN arrCriteria.criteriaType=4 THEN 'Final' WHEN arrCriteria.criteriaType=1 THEN 'Exam' ELSE arrReport.reportName END) as reportTerm, gradeID as grade, 'Standard' as gradeType, (CASE WHEN arrCriteria.criteriaType=4 AND gradeID >= 50.0 THEN gibbonCourse.credits WHEN gradeID = '' THEN '' ELSE 0 END) as creditsAwarded, gibbonCourse.name as courseName, gibbonCourse.nameShort as courseNameShort, (CASE WHEN gibbonCourse.orderBy > 0 THEN gibbonCourse.orderBy ELSE 80 end) as courseOrder
+                FROM gibbonCourse
+                JOIN gibbonCourseClass ON (gibbonCourseClass.gibbonCourseID=gibbonCourse.gibbonCourseID)
+                JOIN gibbonCourseClassPerson ON (gibbonCourseClassPerson.gibbonCourseClassID=gibbonCourseClass.gibbonCourseClassID AND gibbonCourseClassPerson.role='Student')
+                LEFT JOIN arrCriteria ON (gibbonCourse.gibbonCourseID=arrCriteria.subjectID AND (arrCriteria.criteriaType=2 OR arrCriteria.criteriaType=4 OR arrCriteria.criteriaType=1) )
+                LEFT JOIN arrReportGrade ON (arrReportGrade.criteriaID=arrCriteria.criteriaID AND arrReportGrade.studentID = gibbonCourseClassPerson.gibbonPersonID )
+                LEFT JOIN arrReport ON (arrReport.reportID=arrReportGrade.reportID AND arrReport.schoolYearID=gibbonCourse.gibbonSchoolYearID )
+                WHERE gibbonCourse.gibbonSchoolYearID=:gibbonSchoolYearID
+                AND gibbonCourseClassPerson.gibbonPersonID=:gibbonPersonID
+                AND gibbonCourseClass.reportable='Y'
+                AND gibbonCourse.nameShort NOT LIKE '%ECA%'
+                AND gibbonCourse.nameShort NOT LIKE '%HOMEROOM%'
+                AND gibbonCourse.nameShort NOT LIKE '%Advisor%') UNION ALL (
+            SELECT DISTINCT reportTerm, grade, gradeType, creditsAwarded, gibbonCourse.name as courseName, gibbonCourse.nameShort as courseNameShort, (CASE WHEN gibbonCourse.orderBy > 0 THEN gibbonCourse.orderBy ELSE 80 end) as courseOrder
+                FROM arrLegacyGrade
+                JOIN gibbonCourse ON (gibbonCourse.gibbonCourseID=arrLegacyGrade.gibbonCourseID)
+                WHERE arrLegacyGrade.gibbonPersonID = :gibbonPersonID
+                AND arrLegacyGrade.gibbonSchoolYearID= :gibbonSchoolYearID
+                ) ORDER BY courseOrder, courseNameShort";
+
+        return $this->pdo->executeQuery($data, $sql);
+    }
+
+    public function selectStudentEnrolmentByStudent($gibbonPersonID, $yearStart = 7, $yearEnd = 12 ) {
+        $data = array(
+            'gibbonPersonID' => $gibbonPersonID,
+            'yearStart' => $yearStart,
+            'yearEnd' => $yearEnd
+        );
+
+        $sql = "(SELECT gibbonYearGroup.name as yearGroupName, SUBSTRING(gibbonYearGroup.nameShort, 2) as gradeLevel, teacher.surname as teacherSurname, teacher.preferredName as teacherPreferredName, teacher.title as teacherTitle, gibbonSchoolYear.name as schoolYearName, gibbonSchoolYear.status as schoolYearStatus, gibbonSchoolYear.gibbonSchoolYearID as schoolYearID, gibbonSchoolYear.sequenceNumber
+                FROM gibbonPerson
+                JOIN gibbonStudentEnrolment ON (gibbonStudentEnrolment.gibbonPersonID = gibbonPerson.gibbonPersonID)
+                JOIN gibbonRollGroup ON (gibbonRollGroup.gibbonRollGroupID=gibbonStudentEnrolment.gibbonRollGroupID)
+                JOIN gibbonYearGroup ON (gibbonYearGroup.gibbonYearGroupID=gibbonStudentEnrolment.gibbonYearGroupID)
+                JOIN gibbonPerson AS teacher ON (teacher.gibbonPersonID=gibbonRollGroup.gibbonPersonIDTutor)
+                JOIN gibbonSchoolYear ON (gibbonStudentEnrolment.gibbonSchoolYearID=gibbonSchoolYear.gibbonSchoolYearID)
+                WHERE gibbonPerson.gibbonPersonID = :gibbonPersonID
+                AND CAST(SUBSTRING(gibbonYearGroup.nameShort, 2) AS UNSIGNED) >= :yearStart
+                AND CAST(SUBSTRING(gibbonYearGroup.nameShort, 2) AS UNSIGNED) <= :yearEnd) UNION
+            (SELECT '' as yearGroupName, '' as gradeLevel, '' as teacherSurname, '' as teacherPreferredName, '' as teacherTitle, gibbonSchoolYear.name as schoolYearName, gibbonSchoolYear.status as schoolYearStatus, gibbonSchoolYear.gibbonSchoolYearID as schoolYearID, gibbonSchoolYear.sequenceNumber
+                FROM gibbonPerson
+                JOIN arrLegacyGrade ON (arrLegacyGrade.gibbonPersonID=gibbonPerson.gibbonPersonID)
+                JOIN gibbonSchoolYear ON (arrLegacyGrade.gibbonSchoolYearID=gibbonSchoolYear.gibbonSchoolYearID)
+                LEFT JOIN gibbonStudentEnrolment ON (gibbonStudentEnrolment.gibbonPersonID=gibbonPerson.gibbonPersonID && gibbonStudentEnrolment.gibbonSchoolYearID=gibbonSchoolYear.gibbonSchoolYearID)
+                WHERE gibbonPerson.gibbonPersonID = :gibbonPersonID
+                AND gibbonStudentEnrolment.gibbonStudentEnrolmentID IS NULL
+                GROUP BY gibbonSchoolYear.gibbonSchoolYearID )
+                ORDER BY sequenceNumber";
+        return $this->pdo->executeQuery($data, $sql);
+    }
+
+    public function processStudentGrades($enrolment, $gradesData) {
+
+        $grades = array();
+
+        foreach ($gradesData as $row) {
+            if ($this->isTranscriptCourse($row['courseNameShort']) == false) continue;
+
+            $courseGrades = (isset($grades[$row['courseNameShort']]))? $grades[$row['courseNameShort']] : array('Sem1-Mid' => '', 'Sem2-Mid' => '', 'Final' => '', 'credits' => '' );
+
+            // Remove the extra Grade N fron the start of course names
+            $courseName = (!empty($enrolment['yearGroupName']))? str_replace($enrolment['yearGroupName'].' ', '', $row['courseName']) : $row['courseName'];
+
+            // Tranfer Credits
+            if ($row['gradeType'] == 'Transfer' && stristr($courseName, 'transfer') === false ) {
+                $courseName .= (intval($row['creditsAwarded']) > 1)? ' Transfer Credits Awarded' : ' Transfer Credit Awarded';
+            }
+
+            // Retoactive credits
+            if ($row['gradeType'] == 'Retroactive' && stristr($courseName, 'retroactive') === false) {
+                $courseName .= (intval($row['creditsAwarded']) > 1)? ' Retroactive Credits Awarded' : ' Retroactive Credit Awarded';
+            }
+
+            $courseGrades['courseName'] = $courseName;
+            $courseGrades['courseNameShort'] = $row['courseNameShort'];
+
+            $grade = (is_numeric($row['grade']))? round($row['grade']) : $row['grade'];
+            $courseGrades[ $row['reportTerm'] ] = $grade;
+
+            if ($row['reportTerm'] == 'Final') {
+                $defaultCredits = ($enrolment['gradeLevel'] >= 10 && !empty($grade) && intval($grade) < 50)? '0' : '';
+                $credits = (is_numeric($row['creditsAwarded']) && !empty($row['creditsAwarded']) && $enrolment['gradeLevel'] >= 10)? intval($row['creditsAwarded']) : $defaultCredits;
+
+                $courseGrades['credits'] = $credits;
+            }
+
+            $grades[$row['courseNameShort']] = $courseGrades;
+        }
+
+        return $grades;
+    }
+
+    protected function isTranscriptCourse( $courseCode ) {
+
+        if (strstr($courseCode, 'ECA') !== false) return false;
+        if (strstr($courseCode, 'MAM-') !== false) return false;
+        if (strstr($courseCode, 'MTM') !== false) return false;
+        if (strstr($courseCode, 'ERP-') !== false) return false;
+        if (strstr($courseCode, 'ADD-') !== false) return false;
+        if (strstr($courseCode, 'OUT-') !== false) return false;
+        if (strstr($courseCode, 'FIN-') !== false) return false;
+        if (strstr($courseCode, 'ENV-') !== false) return false;
+        if (strstr($courseCode, 'SPRT') !== false) return false;
+        if ($courseCode == 'STUDY') return false;
+        if ($courseCode == 'CHI') return false;
+
+        return true;
     }
 }
