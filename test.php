4801d699796059997b5cb5746f4332e23c8cbe20
Sandra Kuipers <sandra@skuipers.com>
2017-05-04 19:47:53 +0800

    Test file updates

--- test.php
+++ test.php
@@ -17,83 +17,61 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-use Gibbon\Modules\CourseSelection\Timetable\DecisionTree;
+namespace Gibbon\Modules\CourseSelection\Timetable;
+
+use Gibbon\Modules\CourseSelection\Domain\TimetableGateway;
 
 // Autoloader & Module includes
 $loader->addNameSpace('Gibbon\Modules\CourseSelection\\', 'modules/Course Selection/src/');
 
-$startTime = microtime(true);
+ini_set('memory_limit', '1024M');
+ini_set('max_execution_time', 300);
 
 $mockData = array(
-    0 => array(
-        array('nameShort' => 'ENG.A-1', 'period' => 'A1'),
-        array('nameShort' => 'ENG.A-2', 'period' => 'A2'),
-        array('nameShort' => 'ENG.B-1', 'period' => 'B1'),
-        array('nameShort' => 'ENG.B-2', 'period' => 'B2'),
-    ),
-
-    1 => array(
-        array('nameShort' => 'SCI.A-2', 'period' => 'A2'),
-        array('nameShort' => 'SCI.A-3', 'period' => 'A3'),
-        array('nameShort' => 'SCI.B-3', 'period' => 'B3'),
-    ),
-
-    2 => array(
-
-        array('nameShort' => 'MAT.A-3', 'period' => 'A3'),
-        array('nameShort' => 'MAT.A-4', 'period' => 'A4'),
-        array('nameShort' => 'MAT.B-1', 'period' => 'B1'),
-        array('nameShort' => 'MAT.B-4', 'period' => 'B4'),
-    ),
-
-    3 => array(
-        array('nameShort' => 'SST.A-2', 'period' => 'A2'),
-        array('nameShort' => 'SST.B-4', 'period' => 'B4'),
-    ),
-
-    4 => array(
-        array('nameShort' => 'ART.A-1', 'period' => 'A1'),
-        array('nameShort' => 'ART.B-1', 'period' => 'B1'),
-    ),
-
-    5 => array(
-        array('nameShort' => 'PHY.A-1', 'period' => 'A1'),
-        array('nameShort' => 'PHY.B-2', 'period' => 'B2'),
-        array('nameShort' => 'PHY.B-3', 'period' => 'B3'),
-        array('nameShort' => 'PHY.B-4', 'period' => 'B4'),
-    ),
-
-    6 => array(
-        array('nameShort' => 'CTS.A-2', 'period' => 'A2'),
-        array('nameShort' => 'CTS.A-3', 'period' => 'A3'),
-        array('nameShort' => 'CTS.B-3', 'period' => 'B3'),
-    ),
-
-    7 => array(
-        array('nameShort' => 'BIO.A-1', 'period' => 'A1'),
-        array('nameShort' => 'BIO.A-2', 'period' => 'A2'),
-        array('nameShort' => 'BIO.B-2', 'period' => 'B2'),
-    ),
-
-    8 => array(
-        array('nameShort' => 'MUS.A-1', 'period' => 'A1'),
-        array('nameShort' => 'MUS.B-4', 'period' => 'B4'),
-    ),
+    0 => array( 'ENG.A-1', 'ENG.A-2', 'ENG.B-1', 'ENG.B-2' ),
+    1 => array( 'SCI.A-2', 'SCI.A-3', 'SCI.B-3' ),
+    2 => array( 'MAT.A-3', 'MAT.A-4', 'MAT.B-1', 'MAT.B-4' ),
+    3 => array( 'SST.A-2', 'SST.B-4' ),
+    4 => array( 'ART.A-1', 'ART.B-1' ),
+    5 => array( 'PHY.A-1', 'PHY.B-2', 'PHY.B-3', 'PHY.B-4' ),
+    6 => array( 'CTS.A-2', 'CTS.A-3', 'CTS.B-3' ),
+    7 => array( 'BIO.A-1', 'BIO.A-2', 'BIO.B-2' ),
 );
 
-$engine = new DecisionTree();
+// GOAL:
+// Setup the engine
+// Provide environment values
+// Feed the engine some student data
+// Run the engine as a batch process
+// Do something with the results
+// Output some stats & logs
 
-$results = $engine->buildTree($mockData);
 
-$endTime = microtime(true);
+$timetableGateway = new TimetableGateway($pdo);
+
+$engineSettings = new EngineSettings();
+
+$engine = new Engine($engineSettings);
+
+$engine->addData($mockData);
+
+
+$environment = array();
+
+$validator = new Validator($environment);
+$evaluator = new Evaluator($environment);
+$solver = new Solver($validator, $evaluator);
+
+$engine->startEngine($validator, $evaluator, $solver);
+$results = $engine->run();
+
 
 echo '<pre>';
-echo 'Duration: '.($endTime - $startTime).'ms'."\n";
-echo 'Iterations: '.$engine->iterations."\n";
-echo 'Braches Created: '.$engine->branchesCreated."\n";
-echo 'Leaves Created: '.$engine->leavesCreated."\n";
-echo 'Valid Results: '.count($results)."\n";
+echo 'Duration: '.$engine->getRunningTime().'ms'."\n";
+// echo 'Iterations: '.$engine->iterations."\n";
+// echo 'Braches Created: '.$engine->branchesCreated."\n";
+// echo 'Leaves Created: '.$engine->leavesCreated."\n";
+echo 'Valid Results: '.count($results, COUNT_RECURSIVE)."\n";
 echo "\n\n";
-
-print_r($results);
+//print_r($results);
 echo '</pre>';
--- testEngine.php
+++ testEngine.php
@@ -17,15 +17,47 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+namespace Gibbon\Modules\CourseSelection\Timetable;
+
 use Gibbon\Modules\CourseSelection\Domain\TimetableGateway;
-use Gibbon\Modules\CourseSelection\Timetable\Engine;
-use Gibbon\Modules\CourseSelection\Timetable\EngineSettings;
 
 // Autoloader & Module includes
 $loader->addNameSpace('Gibbon\Modules\CourseSelection\\', 'modules/Course Selection/src/');
+
+$mockData = array(
+    0 => array( 'ENG.A-1', 'ENG.A-2', 'ENG.B-1', 'ENG.B-2' ),
+    1 => array( 'SCI.A-2', 'SCI.A-3', 'SCI.B-3' ),
+    2 => array( 'MAT.A-3', 'MAT.A-4', 'MAT.B-1', 'MAT.B-4' ),
+    3 => array( 'SST.A-2', 'SST.B-4' ),
+    4 => array( 'ART.A-1', 'ART.B-1' ),
+    5 => array( 'PHY.A-1', 'PHY.B-2', 'PHY.B-3', 'PHY.B-4' ),
+    6 => array( 'CTS.A-2', 'CTS.A-3', 'CTS.B-3' ),
+    7 => array( 'BIO.A-1', 'BIO.A-2', 'BIO.B-2' ),
+);
+
+// GOAL:
+// Setup the engine
+// Provide environment values
+// Feed the engine some student data
+// Run the engine as a batch process
+// Do something with the results
+// Output some stats & logs
+
 
 $timetableGateway = new TimetableGateway($pdo);
 
 $engineSettings = new EngineSettings();
 
-$engine = new Engine($timetableGateway, $engineSettings);
+$engine = new Engine($engineSettings);
+
+$environment = array();
+
+$validator = new Validator($environment);
+$evaluator = new Evaluator($environment);
+$solver = new Solver($validator, $evaluator);
+
+$engine->startEngine($validator, $evaluator, $solver);
+
+
+
+
