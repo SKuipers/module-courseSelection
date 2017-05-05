e507a168f2958d4d7271fbfa6c09c67c48be7531
Sandra Kuipers <Sandra.Kuipers@tis.edu.mo>
2017-05-05 14:40:55 +0800

    Added basic select statements to TimetableGateway

--- src/Domain/TimetableGateway.php
+++ src/Domain/TimetableGateway.php
@@ -36,4 +36,35 @@ class TimetableGateway
     {
         $this->pdo = $pdo;
     }
+
+    public function selectTimetabledCoursesBySchoolYear($gibbonSchoolYearID)
+    {
+        $data = array('gibbonSchoolYearID' => $gibbonSchoolYearID);
+        $sql = "SELECT CONCAT(gibbonCourse.nameShort,'.',gibbonCourseClass.nameShort) as className, COUNT(DISTINCT gibbonCourseClassPerson.gibbonPersonID) as students, gibbonCourse.name as courseName, gibbonCourse.nameShort as courseNameShort, gibbonCourseClass.nameShort as className, gibbonCourseClass.nameShort as period
+                FROM gibbonCourse
+                JOIN gibbonCourseClass ON (gibbonCourseClass.gibbonCourseID=gibbonCourse.gibbonCourseID)
+                JOIN courseSelectionOffering ON (courseSelectionOffering.gibbonSchoolYearID=gibbonCourse.gibbonSchoolYearID)
+                JOIN courseSelectionOfferingRestriction ON (courseSelectionOfferingRestriction.courseSelectionOfferingID=courseSelectionOffering.courseSelectionOfferingID
+                                                            AND FIND_IN_SET(courseSelectionOfferingRestriction.gibbonYearGroupID, gibbonCourse.gibbonYearGroupIDList))
+                LEFT JOIN gibbonCourseClassPerson ON (gibbonCourseClassPerson.gibbonCourseClassID=gibbonCourseClass.gibbonCourseClassID AND gibbonCourseClassPerson.role='Student')
+                WHERE gibbonCourse.gibbonSchoolYearID=:gibbonSchoolYearID
+                GROUP BY gibbonCourseClass.gibbonCourseClassID";
+
+        return $this->pdo->executeQuery($data, $sql);
+    }
+
+    public function selectApprovedCourseSelectionsBySchoolYear($gibbonSchoolYearID)
+    {
+        $data = array('gibbonSchoolYearID' => $gibbonSchoolYearID);
+        $sql = "SELECT courseSelectionChoice.gibbonPersonIDStudent, gibbonCourse.gibbonCourseID, CONCAT(gibbonCourse.nameShort,'.',gibbonCourseClass.nameShort) as className
+                FROM courseSelectionChoice
+                JOIN courseSelectionApproval ON (courseSelectionApproval.courseSelectionChoiceID=courseSelectionChoice.courseSelectionChoiceID)
+                JOIN gibbonCourse ON (gibbonCourse.gibbonCourseID=courseSelectionChoice.gibbonCourseID)
+                JOIN gibbonCourseClass ON (gibbonCourseClass.gibbonCourseID=gibbonCourse.gibbonCourseID)
+                WHERE courseSelectionChoice.gibbonSchoolYearID=:gibbonSchoolYearID
+                AND courseSelectionChoice.status <> 'Removed'
+                AND courseSelectionChoice.status <> 'Recommended'";
+
+        return $this->pdo->executeQuery($data, $sql);
+    }
 }
