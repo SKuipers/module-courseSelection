0504c40f4b64706cfeddb516e6135933df202951
Sandra Kuipers <Sandra.Kuipers@tis.edu.mo>
2017-05-05 08:54:59 +0800

    PHPified the JS cartesian function

--- moduleFunctions.php
+++ moduleFunctions.php
@@ -17,4 +17,26 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-?>
+/**
+ * From JSfiddle http://jsfiddle.net/9uvfP/ shared by Andrew Chiu
+ */
+function cartesian($args) {
+    $r = array();
+
+    $helper = function($arr, $i, $max) use (&$helper, &$args, &$r) {
+        for ($j=0; $j<count($args[$i]); $j++) {
+            $a = array_slice($arr, 0); // clone arr
+            array_push($a, $args[$i][$j]);
+            if ($i==$max) {
+                array_push($r, $a);
+            } else {
+                $helper($a, $i+1, $max);
+            }
+        }
+    };
+
+    $helper([], 0, count($args)-1);
+
+    return $r;
+};
+
