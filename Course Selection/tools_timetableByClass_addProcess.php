3b865334e6a94ebd1c4e6f6142524f4f2b84e20e
Ray Clark <raynichc@users.noreply.github.com>
2021-05-30 17:07:02 +1000

    Updated process to work with custom blocks

--- Course Selection/tools_timetableByClass_addProcess.php
+++ Course Selection/tools_timetableByClass_addProcess.php
@@ -7,18 +7,18 @@ Copyright (C) 2017, Sandra Kuipers
 include '../../functions.php';
 
 use CourseSelection\Domain\ToolsGateway;
+use Gibbon\Domain\System\SettingGateway;
 
 // Module Bootstrap
 require 'module.php';
 
-$gibbonSchoolYearID = $_REQUEST['gibbonSchoolYearID'] ?? getSettingByScope($connection2, 'Course Selection', 'activeSchoolYear');
+$settingGateway = $container->get(SettingGateway::class);
+
+$gibbonSchoolYearID = $_REQUEST['gibbonSchoolYearID'] ?? $settingGateway->getSettingByScope('Course Selection', 'activeSchoolYear');
 $gibbonCourseClassID = $_REQUEST['gibbonCourseClassID'] ?? '';
 $gibbonTTID = $_REQUEST['gibbonTTID'] ?? '';
-$gibbonTTDayID = $_POST['gibbonTTDayID'] ?? '';
-$gibbonTTColumnRowID = $_POST['gibbonTTColumnRowID'] ?? '';
-$gibbonTTSpaceID = $_POST['gibbonTTSpaceID'] ?? '';
 
-$URL = $_SESSION[$guid]['absoluteURL']."/index.php?q=/modules/Course Selection/tools_timetableByClass.php&gibbonSchoolYearID={$gibbonSchoolYearID}&gibbonTTID={$gibbonTTID}&gibbonCourseClassID={$gibbonCourseClassID}&gibbonTTDayID={$gibbonTTDayID}&gibbonTTColumnRowID={$gibbonTTColumnRowID}&gibbonTTSpaceID={$gibbonTTSpaceID}";
+$URL = $gibbon->session->get('absoluteURL') . "/index.php?q=/modules/Course Selection/tools_timetableByClass.php&gibbonSchoolYearID={$gibbonSchoolYearID}&gibbonTTID={$gibbonTTID}&gibbonCourseClassID={$gibbonCourseClassID}";
 
 if (isActionAccessible($guid, $connection2, '/modules/Course Selection/tools_timetableByClass.php') == false) {
     $URL .= '&return=error0';
@@ -26,33 +26,42 @@ if (isActionAccessible($guid, $connection2, '/modules/Course Selection/tools_tim
     exit;
 } else {
     //Proceed!
-
-    $gibbonTTColumnRowID = strstr($gibbonTTColumnRowID, '-', true);
-
-    if (empty($gibbonCourseClassID) || empty($gibbonTTDayID) || empty($gibbonTTColumnRowID)) {
+    if (empty($gibbonCourseClassID) || empty($gibbonTTID)) {
         $URL .= '&return=error1';
         header("Location: {$URL}");
         exit;
-    } else {
-        $gateway = $container->get('CourseSelection\Domain\ToolsGateway');
+    }
 
-        $data = array(
-            'gibbonTTColumnRowID' => $gibbonTTColumnRowID,
-            'gibbonTTDayID'       => $gibbonTTDayID,
-            'gibbonCourseClassID' => $gibbonCourseClassID,
-            'gibbonSpaceID'       => $gibbonTTSpaceID,
-        );
+    $toolGateway = $container->get(ToolsGateway::class);
 
-        $inserted = $gateway->insertTTDayRowClass($data);
+    $success = $toolGateway->deleteTTDayRowClasses($gibbonCourseClassID, $gibbonTTID);
 
-        if ($inserted == false) {
-            $URL .= '&return=error2';
-            header("Location: {$URL}");
-            exit;
-        } else {
-            $URL .= "&return=success0";
-            header("Location: {$URL}");
-            exit;
+    $entryOrders = $_POST['order'] ?? [];
+
+    foreach ($entryOrders as $order) {
+        $entry = $_POST['ttBlocks'][$order];
+
+        if (empty($entry['gibbonTTColumnRowID']) || empty($entry['gibbonTTDayID'])) {
+            continue;
         }
+
+        $data = [
+            'gibbonTTColumnRowID' => strstr($entry['gibbonTTColumnRowID'], '-', true),
+            'gibbonTTDayID' => $entry['gibbonTTDayID'],
+            'gibbonCourseClassID' => $gibbonCourseClassID,
+            'gibbonSpaceID' => $entry['gibbonTTSpaceID'] ?? ''
+        ]; 
+
+        $success &= $toolGateway->insertTTDayRowClass($data) > 0;
+    }
+
+    if ($success) {
+        $URL .= '&return=success0';
+        header("Location: {$URL}");
+        exit;
+    } else {
+        $URL .= '&return=warning1';
+        header("Location: {$URL}");
+        exit;
     }
 }
